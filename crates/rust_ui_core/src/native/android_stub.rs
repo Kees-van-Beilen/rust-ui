f8d7f24045aa8b43284463f8240586a218af3b72
Kees van Beilen <76178905+Kees-van-Beilen@users.noreply.github.com>
2025-08-09 23:28:26 +0200

    updated android stub

--- crates/rust_ui_core/src/native/android_stub.rs
+++ crates/rust_ui_core/src/native/android_stub.rs
@@ -1,14 +1,36 @@
 pub mod native {
     use crate::view::resources::Resource;
     #[derive(Clone)]
-    pub struct RenderData { }
+    pub struct RenderData {}
     impl RenderData {
         pub fn ament_with<T: Resource, F, K>(&mut self, element: T, with_fn: F) -> K
-        where for<'b> F: FnOnce(RenderData) -> K,
+        where
+            for<'b> F: FnOnce(RenderData) -> K,
         {
             todo!()
         }
     }
-    pub struct MutableView {
+    pub struct MutableView {}
+
+    impl<V> crate::view::mutable::MutableViewRerender for ::std::rc::Rc<::std::cell::RefCell<V>> {
+        fn rerender(&self) {
+            todo!()
+        }
+
+        fn enqueue_change(&self) {
+            todo!()
+        }
+
+        fn flush_changes(&self) {
+            todo!()
+        }
+    }
+
+    pub struct NativeImageHandle {}
+
+    impl NativeImageHandle {
+        pub fn from_path(path:impl ToString)->Self {
+            todo!()
+        }
     }
 }
