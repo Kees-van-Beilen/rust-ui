995f97a5d7e3327b14d7c9d8da69da79a3af1ff2
Kees van Beilen <76178905+Kees-van-Beilen@users.noreply.github.com>
2026-01-31 15:14:31 +0100

    restored the color example

--- examples/macros/colors.rs
+++ examples/macros/colors.rs
@@ -3,44 +3,14 @@ use rust_ui::prelude::*;
 
 #[ui(main)]
 struct RootView {
-    #[state] text:String,
-    #[state] list:Vec<String>,
-
     body:_ = view!{
-        
-       VStack {
-            spacing: Some(10.0),
-
-            ColorView(Color::BLACK).frame(Frame::no_preference().height(100.0))
+        VStack {
+            ColorView(Color::oklch(0.68, 0.2, 29.65))
             HStack {
-                TextField(bind!(text))
-                    .frame(Frame::no_preference().width(300.0))
-                Button("Add to list") || {
-                    list.get_mut().push(text.get().clone());
-                    text.get_mut().clear();
-                }
+                spacing: Some(10.0),
+                ColorView(Color::oklch(0.85, 0.21, 148.24))
+                ColorView(Color::oklch(0.47, 0.22, 263.65))
             }
-
-
-            // ScrollView {
-            //     y:Some(ScrollBehavior::Scroll),
-
-                VStack {
-                    spacing: Some(3.0),
-                    for item in list.get().iter() {
-                        Text(item)
-                            .margin(Margin::all(13.0))
-                            .background {
-                                ColorView(Color::oklcha(1.0, 0.5, 20.0, 1.0))
-                            }
-                        
-                    }
-                }
-            // }
-
-
-            
-
         }
     }
 }
